d8e9e0eb455f49d2c74132313db206cbd8c77954
astrofle <psalas@nrao.edu>
2023-08-22 10:07:26 -0400

    Added optical depth function

--- src/rrlpy/rrl/core.py
+++ src/rrlpy/rrl/core.py
@@ -2,7 +2,9 @@
 Core functions for a generic radio recombination line.
 """
 
-from rrlpy.rrl.constants import Ry, k_B
+import numpy as np
+
+from rrlpy.rrl.constants import Ry, k_B, h, m_e, e, c
 
 
 def fnnp_app(n, dn):
@@ -62,6 +64,58 @@ def mdn(dn):
     return mdn_
 
 
+def tau_constant():
+    """
+    Constants that go into the RRL optical depth.
+    """
+
+    return (
+        h**3 * e**2.0 * np.pi / (np.power(2.0 * np.pi * m_e * k_B, 3.0 / 2.0) * m_e * c)
+    ).cgs
+
+
+def tau_exact(n, ne, te, ni, pl, fnnp, nu, dn, z):
+    """
+    Optical depth of a RRL.
+
+    Parameters
+    ----------
+    n : int
+        Principal quantum number.
+    ne : float
+        Electron density.
+    te : float
+        Electron temperature.
+    ni : float
+        Collisional partner density.
+    pl : float
+        Path length along the line of sight.
+    fnnp : float
+
+    nu : float
+        Frequency of the transition.
+    dn : int
+        Jump between energy levels.
+    z : int
+        Net charge of the atom.
+    """
+
+    cte = tau_constant()
+    xi_ = xi(n, te, z)
+
+    return (
+        cte
+        * n**2
+        * fnnp
+        * ne
+        * ni
+        * pl
+        * np.power(te, -3.0 / 2.0)
+        * np.exp(xi_)
+        * (1.0 - np.exp(-h * nu / (k_B * te)))
+    )
+
+
 def xi(n, te, z):
     """
     Argument of the exponential factor in the Saha-Boltzmann equation.
@@ -81,4 +135,4 @@ def xi(n, te, z):
         :math:`z^2 Ry / (n^2 k_{B} te)`
     """
 
-    return z**2.0 * Ry / (n**2.0 * k_B * te)
+    return (z**2.0 * Ry / (n**2.0 * k_B * te)).cgs
