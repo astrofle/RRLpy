4db0c8da46ac2f64275e5ffd9dda5a44a5a058e6
astrofle <psalas@nrao.edu>
2024-04-02 15:47:51 -0400

    Adds test for layers

---
+++ src/rrlpy/synth/tests/test_layers.py
@@ -0,0 +1,12 @@
+from astropy import units as u
+
+
+class TestLayers:
+    def setup_method(self):
+        self.ne = [1] * u.cm**-3
+        self.te = [1000] * u.K
+        self.l = [1] * u.pc
+        self.v_rms = [1] * u.km / u.s
+
+    def test_layers():
+        pass
